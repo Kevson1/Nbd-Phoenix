120ec92ecf4888e4894b3be61b1acc79eb1b64b0
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-21 13:48:18 +0300

    CREATES THE NEIGHBOURHOOD MODEL AND MAKES THE DATABASE MIGRATION

--- nbd_app/models.py
+++ nbd_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighbourhood(models.Model):
+  neighbourhood_name = models.CharField(max_length=25)
+  general_location = models.CharField(max_length=25)
+  
+  def __str__(self):
+    return self.neighbourhood_name
