71fc330ac7c211bcedb12d7fd1170f1137457ece
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-21 14:00:21 +0300

    CREATES THE PROFILE MODEL

--- nbd_app/models.py
+++ nbd_app/models.py
@@ -8,3 +8,14 @@ class Neighbourhood(models.Model):
   
   def __str__(self):
     return self.neighbourhood_name
+  
+class Profile(models.Model):
+  first_name = models.CharField(max_length=20)
+  last_name = models.CharField(max_length=20)
+  id_number = models.IntegerField()
+  profile_pic = models.ImageField(upload_to='static/images/', null=True)
+  neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.first_name
+  
