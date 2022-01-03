103e74682bd63f16bea3f4e4b8b882a31db887b0
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2022-01-03 15:44:24 +0300

    CREATES THE TEST TEMPLATES FOR THE MODELS

--- nbd_app/tests.py
+++ nbd_app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Neighbourhood, Profile, GeneralPosts, SocialAmenities, Business
 
 # Create your tests here.
+class NeighbourhoodTestCase(TestCase):
+  pass
+
+class ProfileTestCase(TestCase):
+  pass
+
+class SocialAmenitiesTestCase(TestCase):
+  pass
+
+class BusinessTestCase(TestCase):
+  pass
+
+class GeneralPostsTestCase(TestCase):
+  pass
