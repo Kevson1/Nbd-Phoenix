b1f5aebf4bfb70289df39fda2497420800f28f4d
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2022-01-07 19:39:10 +0300

    CREATES A SUPERUSER AND REGISTERS ALL THE MODELS IN THE ADMIN.PY FILE

--- nbd_app/admin.py
+++ nbd_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from nbd_app.models import Business, GeneralPosts, Neighbourhood, Profile, SocialAmenities
+
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(SocialAmenities)
+admin.site.register(GeneralPosts)
--- nbd_app/views.py
+++ nbd_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def businesses(request):
+  
   return render(request, 'index.html')
