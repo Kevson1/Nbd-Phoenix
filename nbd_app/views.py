e27fa0d75f4fddef60fe07dcfcea8cf4636649c6
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2022-01-09 10:08:06 +0300

    CREATES THE GENERAL POSTS TEMPLATE AND ADDS THE LOGIN REQUIRED FUNCTIONALITY

--- nbd_app/views.py
+++ nbd_app/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 from nbd_app.models import Business
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def businesses(request):
   businesses = Business.get_all_businesses()
   return render(request, 'businesses.html', {"businesses":businesses})
 
+@login_required(login_url='/accounts/login/')
 def general_posts(request):
-  pass
+  return render(request,'generalposts.html')
+
+@login_required(login_url='/accounts/login/')
+def social_amenities(request):
+  return render(request, 'socialamenities.html')
